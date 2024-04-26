9ec92431d67572faea25933db9dcc1de10f204cf
ol-voronko <voronyatco@gmail.com>
2024-04-27 00:10:31 +0300

    number:rgb, use .toString

---
+++ lesson-2/06.js
@@ -0,0 +1,7 @@
+let red = prompt("Введіть значення red");
+let green = prompt(" Введіть значення green");
+let blue = prompt("Введіть значення blue");
+
+let color = "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+
+alert(color);
