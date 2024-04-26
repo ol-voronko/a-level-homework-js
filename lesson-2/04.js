0d75d2db8372f596396c3b1f140bd87c177306b1
ol-voronko <voronyatco@gmail.com>
2024-04-26 23:46:44 +0300

    number:divide

---
+++ lesson-2/04.js
@@ -0,0 +1,6 @@
+let first = prompt("Введіть ділене");
+let second = prompt("Введіть дільник");
+
+let result = first / second;
+console.log(c);
+alert("Результат ділення введених чисел : " + Math.floor(result));
