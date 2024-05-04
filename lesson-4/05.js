553936e83d8ef6e03951bbd6d5144bfac9a3b411
ol-voronko <voronyatco@gmail.com>
2024-05-04 06:30:30 +0300

    Comparison:sizes if/else

---
+++ lesson-4/05.js
@@ -0,0 +1,21 @@
+let size = prompt("Введіть ваш розмір білизни");
+
+if (size == 42) {
+  alert("Вaш розмір 36");
+} else if (size == 44) {
+  alert("Вaш розмір 38");
+} else if (size == 46) {
+  alert("Вaш розмір 40");
+} else if (size == 48) {
+  alert("Вaш розмір 42");
+} else if (size == 50) {
+  alert("Вaш розмір 44");
+} else if (size == 52) {
+  alert("Вaш розмір 46");
+} else if (size == 54) {
+  alert("Вaш розмір 48");
+} else if (size == 56) {
+  alert("Вaш розмір 50");
+} else if (size < 42 || size > 56) {
+  alert("Нажаль в нас немає такого розміру");
+}
