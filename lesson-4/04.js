77d366e140e26d2c44e89ed4c6019a1f2b3743a6
ol-voronko <voronyatco@gmail.com>
2024-05-04 05:57:26 +0300

    if/else(confirm)

---
+++ lesson-4/04.js
@@ -0,0 +1,13 @@
+let gender = confirm("Ви жінка?");
+
+if (gender) {
+  alert("Ви-жінка");
+} else {
+  alert("Ви чоловік");
+}
+let today = confirm("зустрінемось сьогодні?");
+if (today) {
+  alert("До зустрічі)");
+} else {
+  alert("Шкода(");
+}
