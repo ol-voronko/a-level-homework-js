cd411aac9954c26da986072676fc85be08767f55
ol-voronko <voronyatco@gmail.com>
2024-05-04 05:45:25 +0300

    boolean:confirm

---
+++ lesson-4/03.js
@@ -0,0 +1,8 @@
+let today = confirm("Сьогодні зустрінемось?");
+console.log(today);
+
+let gender = confirm("Ви жінка?");
+console.log(gender);
+
+let age = confirm("Вaм вже є 18?");
+console.log(age);
