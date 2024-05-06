adae98286ca72f0c70e693fb9357059113285733
ol-voronko <voronyatco@gmail.com>
2024-05-06 13:57:56 +0300

    дубль3

--- lesson-4/13.js
+++ lesson-4/13.js
@@ -6,28 +6,31 @@ let plnPurchase = 9.71;
 let plnSale = 9.875;
 let rate;
 let currency = prompt("Введіть назву валюти для обміну: USD,EUR,PLN");
-
-let operation = confirm(
-  "Якщо бажаєте купити натисніть ОК,якщо бажаєте продати натисніть Cansel(Скасувати)"
-);
-
-if (currency.toUpperCase() === "USD") {
-  rate = operation ? usdSale : usdPurchase;
-} else if (currency.toUpperCase() === "EUR") {
-  rate = operation ? eurSale : eurPurchase;
-} else if (currency.toUpperCase() === "PLN") {
-  rate = operation ? plnSale : plnPurchase;
+if (
+  currency.toUpperCase() !== "USD" &&
+  currency.toUpperCase() !== "EUR" &&
+  currency.toUpperCase() !== "PLN"
+) {
+  alert("Некорректно вказано назву валюти!Почніть спочатку");
 } else {
-  alert("Некорректно вказано назву валюти!");
-}
+  let operation = confirm(
+    "Якщо бажаєте купити натисніть ОК,якщо бажаєте продати натисніть Cansel(Скасувати)"
+  );
 
-let sum = prompt("Введіть суму обміну");
-let result = operation ? sum / rate : sum * rate;
-
-if (!isNaN(result)) {
-  operation
-    ? alert(`Ви отримаєте ${result.toFixed(2)} ${currency.toUpperCase()}`)
-    : alert(`Ви отримаєте ${result.toFixed(2)} грн`);
-} else {
-  alert("Почніть спочатку");
+  if (currency.toUpperCase() === "USD") {
+    rate = operation ? usdSale : usdPurchase;
+  } else if (currency.toUpperCase() === "EUR") {
+    rate = operation ? eurSale : eurPurchase;
+  } else if (currency.toUpperCase() === "PLN") {
+    rate = operation ? plnSale : plnPurchase;
+  }
+  let sum = prompt("Введіть суму обміну");
+  if (!isNaN(sum)) {
+    alert("Почніть спочатку,бо не вказана сума для обміну");
+  } else {
+    let result = operation ? sum / rate : sum * rate;
+    operation
+      ? alert(`Ви отримаєте ${result.toFixed(2)} ${currency.toUpperCase()}`)
+      : alert(`Ви отримаєте ${result.toFixed(2)} грн`);
+  }
 }
--- lesson-4/14.js
+++ lesson-4/14.js
@@ -1,25 +1,30 @@
 let userMove = prompt("Ваш хід:камінь,ножиці чи папір?");
-// некорректно  відпрацьовує
-// if (userMove.toLowerCase() !== "камінь" || "ножиці" || "папір") {
-//   alert("Будьте уважні!Ви ввели некорректне значення і програли(");
-// }
-let botMove = Math.floor(Math.random() * 3);
-if (botMove == 0) {
-  botMove = "камінь";
-} else if (botMove == 1) {
-  botMove = "ножиці";
-} else if (botMove == 2) {
-  botMove = "папір";
-}
-alert(`Хід комп'ютера:${botMove}`);
-if (userMove.toLowerCase() === botMove) {
-  alert("нічия");
-} else if (
-  (userMove.toLowerCase() === "камінь" && botMove === "ножиці") ||
-  (userMove.toLowerCase() === "ножиці" && botMove === "папір") ||
-  (userMove.toLowerCase() === "папір" && botMove === "камінь")
+
+if (
+  userMove.toLowerCase() !== "камінь" &&
+  userMove.toLowerCase() !== "ножиці" &&
+  userMove.toLowerCase() !== "папір"
 ) {
-  alert("Вітаємо з виграшем!");
+  alert("Будьте уважні!Ви ввели некорректне значення і програли(");
 } else {
-  alert("Ви програли(");
+  let botMove = Math.floor(Math.random() * 3);
+  if (botMove == 0) {
+    botMove = "камінь";
+  } else if (botMove == 1) {
+    botMove = "ножиці";
+  } else if (botMove == 2) {
+    botMove = "папір";
+  }
+  alert(`Хід комп'ютера:${botMove}`);
+  if (userMove.toLowerCase() === botMove) {
+    alert("нічия");
+  } else if (
+    (userMove.toLowerCase() === "камінь" && botMove === "ножиці") ||
+    (userMove.toLowerCase() === "ножиці" && botMove === "папір") ||
+    (userMove.toLowerCase() === "папір" && botMove === "камінь")
+  ) {
+    alert("Вітаємо з виграшем!");
+  } else {
+    alert("Ви програли(");
+  }
 }
