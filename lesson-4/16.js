5a10c8b335351b536a0df3bcf1bac6d3db4803c8
ol-voronko <voronyatco@gmail.com>
2024-05-06 12:09:31 +0300

    Завдання на чорний пояс)))

---
+++ lesson-4/16.js
@@ -0,0 +1,23 @@
+let userMove = prompt("Ваш хід:камінь,ножиці чи папір?");
+let botMove = Math.floor(Math.random() * 3);
+if (botMove == 0) {
+  botMove = "камінь";
+} else if (botMove == 1) {
+  botMove = "ножиці";
+} else if (botMove == 2) {
+  botMove = "папір";
+}
+alert(`Хід комп'ютера:  ${botMove}`);
+
+(userMove.toLowerCase() === botMove && alert("нічия")) ||
+  (((userMove.toLowerCase() === "камінь" && botMove === "ножиці") ||
+    (userMove.toLowerCase() === "ножиці" && botMove === "папір") ||
+    (userMove.toLowerCase() === "папір" && botMove === "камінь")) &&
+    alert("Вітаємо з виграшем!")) ||
+  (userMove.toLowerCase() !== botMove &&
+    !(
+      (userMove.toLowerCase() === "камінь" && botMove === "ножиці") ||
+      (userMove.toLowerCase() === "ножиці" && botMove === "папір") ||
+      (userMove.toLowerCase() === "папір" && botMove === "камінь")
+    ) &&
+    alert("Ви програли!"));
