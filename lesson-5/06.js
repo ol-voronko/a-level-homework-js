148904e5b78f9bcc58c278dd389711fac8f7842e
ol-voronko <voronyatco@gmail.com>
2024-05-09 12:15:29 +0300

    slice

---
+++ lesson-5/06.js
@@ -0,0 +1,9 @@
+const arr = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 2, 3, 4],
+  [0, 2, 4, 6, 8],
+  [0, 3, 6, 9, 12],
+  [0, 4, 8, 12, 16],
+];
+const arrWithoutNulls = arr.slice(-4);
+console.log(arrWithoutNulls);
