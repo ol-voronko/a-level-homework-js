76249a944d85e8967aa14f2dad720726874ee80a
ol-voronko <voronyatco@gmail.com>
2024-05-09 16:47:06 +0300

    without all nulles

--- lesson-5/06.js
+++ lesson-5/06.js
@@ -6,4 +6,8 @@ const arr = [
   [0, 4, 8, 12, 16],
 ];
 const arrWithoutNulls = arr.slice(-4);
+arrWithoutNulls[0] = arrWithoutNulls[0].slice(-4);
+arrWithoutNulls[1] = arrWithoutNulls[1].slice(-4);
+arrWithoutNulls[2] = arrWithoutNulls[2].slice(-4);
+arrWithoutNulls[3] = arrWithoutNulls[3].slice(-4);
 console.log(arrWithoutNulls);
---
+++ lesson-5/10.js
@@ -0,0 +1,3 @@
+let arr = [1, 2, 3, 4, 5];
+let arr2 = arr;
+console.log(arr === arr2);
