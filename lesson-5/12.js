30df48c3909399b7c8dae346008b4f6a53c27397
ol-voronko <voronyatco@gmail.com>
2024-05-09 17:51:19 +0300

    destruct

---
+++ lesson-5/12.js
@@ -0,0 +1,4 @@
+let string = prompt("Введіть якесь слово");
+const [first, , , , fifth, , , , nineth] = string;
+console.log(string);
+alert(`first : ${first}, fifth : ${fifth}, nineth : ${nineth}`);
