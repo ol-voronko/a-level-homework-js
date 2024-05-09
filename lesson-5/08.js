34e47e67db3c22bbca196344e7ac91b8097c9002
ol-voronko <voronyatco@gmail.com>
2024-05-09 13:02:50 +0300

    reverse(push/pop)

---
+++ lesson-5/08.js
@@ -0,0 +1,15 @@
+const first = [];
+first.push(prompt("Введіть якесь слово"));
+first.push(prompt("Введіть якесь слово"));
+first.push(prompt("Введіть якесь слово"));
+first.push(prompt("Введіть якесь слово"));
+first.push(prompt("Введіть якесь слово"));
+console.log(first);
+
+const second = [];
+second.push(first.pop());
+second.push(first.pop());
+second.push(first.pop());
+second.push(first.pop());
+second.push(first.pop());
+console.log(second);
