a682b1b4962bbc771ea6d1967a1ab03adfcd3b21
ol-voronko <voronyatco@gmail.com>
2024-05-10 20:12:41 +0300

    reduce html

---
+++ lesson-5/25.js
@@ -0,0 +1,6 @@
+const currencies = ["USD", "EUR", "GBP", "UAH"];
+let str = "<select>";
+
+str += currencies.reduce((a, b) => a + "<option>" + b + "</option>", "");
+str += "</select>";
+document.write(str);
