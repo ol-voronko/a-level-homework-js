7983a7218ecd41cb0ef2bc89c06b5a3b914285f3
ol-voronko <voronyatco@gmail.com>
2024-05-13 10:18:32 +0300

    Html tree

---
+++ lesson-6/02.js
@@ -0,0 +1,52 @@
+let body = {
+  tagName: "body",
+  children: [
+    {
+      tagName: "div",
+      children: [
+        {
+          tagName: "span",
+          children: ["Enter a data please: "],
+        },
+        {
+          tagName: "br",
+        },
+        {
+          tagName: "input",
+          attrs: {
+            type: "text",
+            id: "name",
+          },
+        },
+        {
+          tagName: "input",
+          attrs: {
+            type: "text",
+            id: "surname",
+          },
+        },
+      ],
+    },
+    {
+      tagName: "div",
+      children: [
+        {
+          tagName: "button",
+          attrs: {
+            id: "ok",
+          },
+          children: ["OK"],
+        },
+        {
+          tagName: "button",
+          attrs: {
+            id: "cancel",
+          },
+          children: ["Cancel"],
+        },
+      ],
+    },
+  ],
+};
+console.log(body.children[1].children[1].children);
+console.log(body.children[0].children[3].attrs.id);
