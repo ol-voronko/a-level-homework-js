d1983170585db48d298a12b865224d9d3945368a
ol-voronko <voronyatco@gmail.com>
2024-05-14 18:17:13 +0300

    currency rates right

--- lesson-6/07.js
+++ lesson-6/07.js
@@ -1,18 +1,18 @@
 fetch("https://open.er-api.com/v6/latest/USD")
   .then((res) => res.json())
   .then((data) => {
-    //ця функція запускається коли дані завантажуються.
-    //Інший код працює РАНIШЕ.
-    //тільки тут є змінна data з завантаженими даними
-    console.log(data); // Вивчіть структуру, що отримується з сервера в консолі
+    console.log(data);
     let currencyRate =
       data.rates[prompt("Введіть вхідну валюту ").toUpperCase()];
-
+    console.log(currencyRate);
     let resultRate =
       data.rates[prompt("Введіть валюту ,яку хочете отримати").toUpperCase()];
+    console.log(resultRate);
     let sum = prompt("Введіть суму обміну");
-    result = (sum * currencyRate) / resultRate;
+    result = (sum / currencyRate) * resultRate;
     alert(`Ви отримаєте  ${result.toFixed(2)}`);
 
     return result;
   });
+// 1000 uah   1$ = 39 Uah 1$ = 2 EUR
+// 1000/39 = 300 $
