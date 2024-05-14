a7ec1de29339a8f82451bf85ef9417831478f992
ol-voronko <voronyatco@gmail.com>
2024-05-14 11:20:49 +0300

    -

--- lesson-3/10.js
+++ lesson-3/10.js
@@ -1,4 +1,4 @@
 const regexp =
   /http(?: s ?): \/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
 let videoLink = "https://youtu.be/c9z_3VhNefs?si=WNagR6uJDIHFeZ8X";
-console.log(videoLink.match[regexp]);
+// щось пішло нетак(
