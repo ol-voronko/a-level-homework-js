a4a8fb8c3fb8260e4a644f4a547ceb7b90643040
ol-voronko <voronyatco@gmail.com>
2024-05-20 20:50:49 +0300

    for table

--- lesson-7/08.js
+++ lesson-7/08.js
@@ -1,10 +1,10 @@
 let createTable = (arrOfArrays) => {
   let str = '<table style="border: 1px solid black;border-collapse:collapse;">';
-  for (const array of arrays) {
+  for (const array of arrOfArrays) {
     str +=
-      arrays.indexOf(array) % 2 !== 0
-        ? '<tr style="background-color:blue;border: 1px solid black;border-collapse:collapse;">'
-        : '<tr style="background-color:yellow;border: 1px solid black;border-collapse:collapse;">';
+      arrOfArrays.indexOf(array) % 2 !== 0
+        ? '<tr style="background-color:lightblue;border: 1px solid black;border-collapse:collapse;">'
+        : '<tr style="background-color:lightgrey;border: 1px solid black;border-collapse:collapse;">';
 
     for (const number of array) {
       str += `<td style="border: 1px solid black;border-collapse:collapse;"> ${number} </td>`;
