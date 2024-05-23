0cc337618b66467655eedad3575a2b7ebf6dda6e
ol-voronko <voronyatco@gmail.com>
2024-05-23 15:51:53 +0300

    switch : sizes

---
+++ lesson-8/03.js
@@ -0,0 +1,29 @@
+let size = prompt("Введіть ваш розмір білизни");
+switch (size) {
+  case "42":
+    alert("Вaш розмір 36");
+    break;
+  case "44":
+    alert("Вaш розмір 38");
+    break;
+  case "46":
+    alert("Вaш розмір 40");
+    break;
+  case "48":
+    alert("Вaш розмір 42");
+    break;
+  case "50":
+    alert("Вaш розмір 44");
+    break;
+  case "52":
+    alert("Вaш розмір 46");
+    break;
+  case "54":
+    alert("Вaш розмір 48");
+    break;
+  case "56":
+    alert("Вaш розмір 50");
+    break;
+  default:
+    alert("Нажаль в нас немає такого розміру");
+}
