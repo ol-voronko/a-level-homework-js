eeadb027aa4d7f9bdbdbd0a62cb8a0bafdadae2c
ol-voronko <voronyatco@gmail.com>
2024-06-07 12:29:49 +0300

    createPersonClosure

--- lesson-10/03.js
+++ lesson-10/03.js
@@ -18,23 +18,46 @@ function createPersonClosure(name, surname) {
     return `${name} ${fatherName || ""} ${surname}`;
   }
   function setName(newName) {
-    newName ===
-      newName.slice(0, 1).toUpperCase() + newName.slice(1).toLowerCase() &&
-      getName((name = newName));
+    if (
+      newName &&
+      typeof newName === "string" &&
+      newName ===
+        newName.slice(0, 1).toUpperCase() + newName.slice(1).toLowerCase()
+    ) {
+      name = newName;
+    }
+    return name;
   }
 
   function setSurname(newSurname) {
-    newSurname ===
-      newSurname.slice(0, 1).toUpperCase() +
-        newSurname.slice(1).toLowerCase() && (surname = newSurname);
+    if (
+      newSurname &&
+      typeof newSurname === "string" &&
+      newSurname ===
+        newSurname.slice(0, 1).toUpperCase() + newSurname.slice(1).toLowerCase()
+    ) {
+      surname = newSurname;
+    }
+    return surname;
   }
   function setFatherName(newFatherName) {
-    newFatherName ===
-      newFatherName.slice(0, 1).toUpperCase() +
-        newFatherName.slice(1).toLowerCase() && (fatherName = newFatherName);
+    if (
+      newFatherName &&
+      typeof newFatherName === "string" &&
+      newFatherName ===
+        newFatherName.slice(0, 1).toUpperCase() +
+          newFatherName.slice(1).toLowerCase()
+    ) {
+      fatherName = newFatherName;
+    }
+    return fatherName;
   }
   function setAge(newAge) {
-    newAge > 0 && newAge < 100 && (age = newAge);
+    newAge = +newAge;
+    if (newAge > 0 && newAge < 100) {
+      age = newAge;
+    }
+    return age;
   }
   function setFullName(newFullName) {
     const [newSurname, newName, newFatherName] = newFullName.split(" ");
@@ -55,7 +78,3 @@ function createPersonClosure(name, surname) {
     setFullName,
   };
 }
-const b = createPersonClosure("Ганна", "Іванова");
-console.log(b.getFullName());
-b.setFullName("VBNMN Ганна cnnmbcxnbm");
-console.log(b.getSurname());
