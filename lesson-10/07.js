380bffaa00a1c76935b1ae846a6f295a4cd70a91
ol-voronko <voronyatco@gmail.com>
2024-06-08 18:09:09 +0300

    getSetForm

--- lesson-10/07.js
+++ lesson-10/07.js
@@ -78,3 +78,85 @@ function getSetForm(parent, getSet) {
     }
   }
 }
+function createPersonClosure(name, surname) {
+  let age = 1,
+    fatherName = "";
+  function getName() {
+    return name;
+  }
+  function getSurname() {
+    return surname;
+  }
+
+  function getFatherName() {
+    return fatherName;
+  }
+
+  function getAge() {
+    return age;
+  }
+  function getFullName() {
+    return `${name} ${fatherName || ""} ${surname}`;
+  }
+  function setName(newName) {
+    if (
+      newName &&
+      typeof newName === "string" &&
+      newName ===
+        newName.slice(0, 1).toUpperCase() + newName.slice(1).toLowerCase()
+    ) {
+      name = newName;
+    }
+    return name;
+  }
+
+  function setSurname(newSurname) {
+    if (
+      newSurname &&
+      typeof newSurname === "string" &&
+      newSurname ===
+        newSurname.slice(0, 1).toUpperCase() + newSurname.slice(1).toLowerCase()
+    ) {
+      surname = newSurname;
+    }
+    return surname;
+  }
+  function setFatherName(newFatherName) {
+    if (
+      newFatherName &&
+      typeof newFatherName === "string" &&
+      newFatherName ===
+        newFatherName.slice(0, 1).toUpperCase() +
+          newFatherName.slice(1).toLowerCase()
+    ) {
+      fatherName = newFatherName;
+    }
+    return fatherName;
+  }
+  function setAge(newAge) {
+    newAge = +newAge;
+    if (newAge > 0 && newAge < 100) {
+      age = newAge;
+    }
+    return age;
+  }
+  function setFullName(newFullName) {
+    const [newName, newFatherName, newSurname] = newFullName.split(" ");
+    setName(newName);
+    setSurname(newSurname);
+    setFatherName(newFatherName);
+  }
+  return {
+    getName,
+    getSurname,
+    getFatherName,
+    getAge,
+    getFullName,
+    setName,
+    setSurname,
+    setFatherName,
+    setAge,
+    setFullName,
+  };
+}
+getSetForm(document.body, createPersonClosure("Ганна", "Іванова"));
