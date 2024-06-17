c96a9d2b5d7ad38ead0b2853b36615a8e024c2a7
ol-voronko <voronyatco@gmail.com>
2024-06-17 18:20:11 +0300

    delete

--- lesson-13(Constructor-ФООП)/01.js
+++
@@ -1,6 +0,0 @@
-function Person(name, surname) {
-  this.name = () => name;
-  this.surname = () => surname;
-  this.getFullName = () =>
-    `${this.name()} ${this.fatherName || ""} ${this.surname()}`;
-}
