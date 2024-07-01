e5b4af52c3f8f44d03ac600b11c30384c7bf8152
ol-voronko <voronyatco@gmail.com>
2024-07-01 18:36:51 +0300

    SwapiLinks

--- lesson-16(Async,await)/02.js
+++ lesson-16(Async,await)/02.js
@@ -1,27 +1,32 @@
 async function swapiLinks(link) {
   let response = await fetch(link);
   let responseData = await response.json();
-
+  let promises = [];
   for (const key of Object.keys(responseData)) {
     if (
       typeof responseData[key] === "string" &&
       responseData[key].includes("https://")
     ) {
-      fetch(`${responseData[key]}`)
+      responseData[key] = fetch(`${responseData[key]}`)
         .then((res) => res.json())
         .then((res) => (responseData[key] = res));
+      promises.push(responseData[key]);
     } else if (Array.isArray(responseData[key])) {
-      responseData[key] = responseData[key].map(async (el) => {
+      responseData[key] = responseData[key].map((el) => {
         if (el.startsWith("https://")) {
-          const response = await fetch(`${el}`).then((res) => res.json());
-          return response;
+          return fetch(`${el}`).then((res) => res.json());
         } else {
           return el;
         }
       });
-      await Promise.all(responseData[key]);
+      const promise = Promise.all(responseData[key]).then(
+        (data) => (responseData[key] = data)
+      );
+      promises.push(promise);
     }
   }
+  await Promise.all(promises);
+
   return responseData;
 }
 swapiLinks("https://swapi.dev/api/people/20/").then((yodaWithLinks) =>
