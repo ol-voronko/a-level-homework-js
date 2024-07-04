9a16a34db82bd9a9cbdfc5e93fdf9889d59702c2
ol-voronko <voronyatco@gmail.com>
2024-07-04 19:26:14 +0300

    Promise race

--- lesson-15(Promise)/03.js
+++ lesson-15(Promise)/03.js
@@ -9,10 +9,7 @@ Promise.race([
   fetch("https://swapi.dev/api/people/1/")
     .then((res) => res.json())
     .then((luke) => console.log(luke)),
-  delay(250)
-    .then((result) =>
-      console.log(`Секунда пройшла. Результат промісу: ${result}`)
-    )
-    .then(() => delay(1000))
-    .then((ms) => console.log(`І ще дві секунди минуло: ${ms}`)),
-]).then((value) => console.log(value));
+  delay(250),
+]).then((value) => {
+  console.log(value);
+});
