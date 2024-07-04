51490604ff193e1d276e922d986040aab321de0c
ol-voronko <voronyatco@gmail.com>
2024-07-04 16:21:05 +0300

    jwtDecode

---
+++ lesson-17(REST,GraphQL,JWT)/07.js
@@ -0,0 +1,25 @@
+function jwtDecode(token) {
+  try {
+    const idPart = token.split(".")[1];
+    const decoded = window.atob(idPart);
+    const result = JSON.parse(decoded);
+    return result;
+  } catch (err) {
+    return undefined;
+  }
+}
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiaWQiOiI2MzIyMDVhZWI3NGUxZjVmMmVjMWEzMjAiLCJsb2dpbiI6InRlc3Q0NTciLCJhY2wiOlsiNjMyMjA1YWViNzRlMWY1ZjJlYzFhMzIwIiwidXNlciJdfSwiaWF0IjoxNjY4MjcyMTYzfQ.rxV1ki9G6LjT2IPWcqkMeTi_1K9sb3Si8vLB6UDAGdw";
+console.log(jwtDecode(token));
+try {
+  console.log(jwtDecode()); //undefined
+  console.log(jwtDecode("дічь")); //undefined
+  console.log(jwtDecode("ey.ey.ey")); //undefined
+
+  console.log(
+    "до сюди допрацювало, а значить jwtDecode не матюкався в консоль червоним кольором"
+  );
+} finally {
+  console.log("ДЗ, мабуть, закінчено");
+}
+console.log(jwtDecode());
