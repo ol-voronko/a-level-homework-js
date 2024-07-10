bb9bd51a286a24cfe1687a567b88d781e59e6e44
ol-voronko <voronyatco@gmail.com>
2024-07-10 08:58:38 +0300

    jwtDecode

--- lesson-17(REST,GraphQL,JWT)/07.js
+++ lesson-17(REST,GraphQL,JWT)/07.js
@@ -8,6 +8,18 @@ function jwtDecode(token) {
     return undefined;
   }
 }
+
+// АБО
+// як краще писати ?Перший варіант здається більш зрозумілим
+
+// function jwtDecode(token) {
+//   try {
+//     const result = JSON.parse(window.atob(token.split(".")[1]));
+//     return result;
+//   } catch (err) {
+//     return undefined;
+//   }
+// }
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiaWQiOiI2MzIyMDVhZWI3NGUxZjVmMmVjMWEzMjAiLCJsb2dpbiI6InRlc3Q0NTciLCJhY2wiOlsiNjMyMjA1YWViNzRlMWY1ZjJlYzFhMzIwIiwidXNlciJdfSwiaWF0IjoxNjY4MjcyMTYzfQ.rxV1ki9G6LjT2IPWcqkMeTi_1K9sb3Si8vLB6UDAGdw";
 console.log(jwtDecode(token));
